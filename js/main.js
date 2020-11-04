3b435558253ccdf708612e87f08f5a250b444634
cherubina-pantano <cherubina.pantano@gmail.com>
2020-11-04 16:54:50 +0100

    Print icons b/w

--- js/main.js
+++ js/main.js
@@ -99,8 +99,36 @@ $(document).ready( function () {
       },
   ];
 
+  // Icons container
+  const container = $('.icons');
+
+  // Stampa icone
+  printIcons(icons, container);
 
 
 
 
-});
+
+}); // <-- end doc ready
+
+
+// FUNZIONI
+// PRINT ICONS
+function printIcons(icons, container) {
+  // iteriamo sull'array e come parametro ho l'icona(=element)
+  icons.forEach((icon) => {
+    // destrutturo e creo le nuove variabili
+    const {family, prefix, name} = icon;
+    // cosa inserisco nel doc
+    const html =
+    `<div class="icon padd-20">
+      <i class="${family} ${prefix}${name}"></i>
+      <div class="title">
+        ${name}
+      </div>
+    </div>`;
+
+    container.append(html)
+  });
+
+}
